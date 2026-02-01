e527c96145fd8b3c06a8cb99d3ab4ca0c3b60b12
A-PachecoT <andre.pacheco.t@uni.pe>
2026-02-01 16:15:46 -0500

    fix: update BASE_URL for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://cofoundy.github.io",
-  base: "/portfolio-annie-sandoval",
+  site: "https://anniesltcoach.com",
+  base: "/",
   vite: {
     plugins: [tailwindcss()],
   },
